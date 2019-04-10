89ddabe509f16024c0b638ff307115bd915e7884
Rafael Jaber <rafael.jaber@hotmail.com>
2019-04-10 11:28:51 +0000

    Primeiro Commit do dia

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import (
+    Brand, 
+    Vehicle, 
+    Person, 
+    Parameters, 
+    MoveRotary
+)
 
-# Register your models here.
+admin.site.register(Brand)
+admin.site.register(Vehicle)
+admin.site.register(Person)
+admin.site.register(Parameters)
+admin.site.register(MoveRotary)
--- core/models.py
+++ core/models.py
@@ -6,13 +6,44 @@ class Person(models.Model):
     adress = models.CharField(max_length=200)
     phone = models.CharField(max_length=20)
 
+    def __str__(self):
+        return self.name
+    
+
 
 class Brand(models.Model):
     name= models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.name
+    
 
 class Vehicle(models.Model):
     brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
     board = models.CharField(max_length=7)
+    ouwner = models.ForeignKey(Person, on_delete=models.CASCADE)
     color = models.CharField(max_length=15)
     note = models.TextField()
+
+    def __str__(self): 
+        return self.brand.name + ' - ' + self.board
+
+
+class Parameters(models.Model):
+    value_hour = models.DecimalField(max_digits=5, decimal_places=2)
+    value_month = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return "Parametros Gerais"
+
+
+class MoveRotary(models.Model):
+    checkin = models.DateTimeField(auto_now=False)
+    checkout = models.DateTimeField(auto_now=False, null=True, blank=True)
+    value_our = models.DecimalField(max_digits=5, decimal_places=2)
+    vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
+    paid = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.vehicle.board
+    
